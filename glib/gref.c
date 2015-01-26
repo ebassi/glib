3b5cacc950aaca73fe23cfc71406927732e81405
Emmanuele Bassi <ebassi@gnome.org>
2015-01-26 12:26:38 +0000

    Add reference counted memory areas
    A lot of our data structures are reference counted these days. It can be
    useful to allow creating reference counted data without necessarily
    duplicating all the reference counting machinery — a field in the
    struct, reference and unreference functions, a GDestroyNotify-compatible
    function for destroying the contents.
    
    We can re-use the same overallocation and pointer offset trick we use
    when allocating a GTypeInstance in gobject, and add all the reference
    counting meta-information directly on top of the allocation size.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=622721

---
+++ glib/gref.c
@@ -0,0 +1,351 @@
+/* gref.h: Reference counted memory areas
+ *
+ * Copyright © 2015  Emmanuele Bassi
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the licence, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * SECTION:refcount
+ * @short_description: reference counted memory areas
+ * @title: References
+ *
+ * These functions provide support for allocating and freeing reference
+ * counted memory areas.
+ *
+ * Reference counted memory areas are kept alive as long as something holds
+ * a reference on them; as soon as their reference count drops to zero, the
+ * associated memory is freed.
+ */
+
+#include "config.h"
+#include "glibconfig.h"
+
+#include "gref.h"
+
+#include "gmessages.h"
+#include "valgrind.h"
+
+#include <string.h>
+
+#define STRUCT_ALIGNMENT        (2 * sizeof (gsize))
+#define ALIGN_STRUCT(offset)    ((offset + (STRUCT_ALIGNMENT - 1)) & -STRUCT_ALIGNMENT)
+
+#define G_REF_SIZE              sizeof (GRef)
+#define G_REF(ptr)              (GRef *) (((gchar *) (ptr)) - G_REF_SIZE)
+
+typedef struct _GRef    GRef;
+
+struct _GRef
+{
+  volatile int ref_count;
+
+  gsize alloc_size;
+
+  GDestroyNotify notify;
+
+  guint is_atomic : 1;
+};
+
+#ifdef G_ENABLE_DEBUG
+#include "ghash.h"
+#include "gthread.h"
+
+static GHashTable *referenced_pointers;
+G_LOCK_DEFINE_STATIC (referenced_pointers);
+
+static inline void
+g_ref_register (gpointer ref)
+{
+  G_LOCK (referenced_pointers);
+  if (G_UNLIKELY (referenced_pointers == NULL))
+    referenced_pointers = g_hash_table_new (NULL, NULL);
+
+  g_hash_table_add (referenced_pointers, ref);
+  G_UNLOCK (referenced_pointers);
+}
+
+static inline void
+g_ref_unregister (gpointer ref)
+{
+  G_LOCK (referenced_pointers);
+  if (G_LIKELY (referenced_pointers != NULL))
+    g_hash_table_remove (referenced_pointers, ref);
+  G_UNLOCK (referenced_pointers);
+}
+
+static gboolean
+g_is_ref (gpointer ref)
+{
+  gboolean res = FALSE;
+
+  G_LOCK (referenced_pointers);
+  if (G_LIKELY (referenced_pointers != NULL))
+    res = g_hash_table_contains (referenced_pointers, ref);
+  G_UNLOCK (referenced_pointers);
+
+  return res;
+}
+#endif
+
+/**
+ * g_ref_destroy:
+ * @ref: a reference counted memory area
+ *
+ * Forces the destruction of a reference counter memory area.
+ *
+ * Since: 2.44
+ */
+void
+g_ref_destroy (gpointer ref)
+{
+  GRef *real = G_REF (ref);
+  gsize alloc_size = real->alloc_size;
+  gsize private_size = G_REF_SIZE;
+  gchar *allocated = ((gchar *) ref) - private_size;
+
+  if (real->notify != NULL)
+    real->notify (ref);
+
+#ifdef G_ENABLE_DEBUG
+  g_ref_unregister (ref);
+#endif
+
+  if (RUNNING_ON_VALGRIND)
+    {
+      private_size += ALIGN_STRUCT (1);
+      allocated -= ALIGN_STRUCT (1);
+
+      *(gpointer *) (allocated + private_size + alloc_size) = NULL;
+      g_free (allocated);
+    }
+  else
+    g_free (allocated);
+}
+
+static gpointer
+g_ref_alloc_internal (gsize          alloc_size,
+                      gboolean       atomic,
+                      gboolean       clear,
+                      GDestroyNotify notify)
+{
+  gsize private_size = G_REF_SIZE;
+  gchar *allocated;
+  GRef *real;
+
+  g_return_val_if_fail (alloc_size != 0, NULL);
+
+  if (RUNNING_ON_VALGRIND)
+    {
+      private_size += ALIGN_STRUCT (1);
+
+      if (clear)
+        allocated = g_malloc0 (private_size + alloc_size + sizeof (gpointer));
+      else
+        allocated = g_malloc (private_size + alloc_size + sizeof (gpointer));
+
+      *(gpointer *) (allocated + private_size + alloc_size) = allocated + ALIGN_STRUCT (1);
+
+      VALGRIND_MALLOCLIKE_BLOCK (allocated + private_size, alloc_size + sizeof (gpointer), 0, TRUE);
+      VALGRIND_MALLOCLIKE_BLOCK (allocated + ALIGN_STRUCT (1), private_size - ALIGN_STRUCT (1), 0, TRUE);
+    }
+  else
+    {
+      if (clear)
+        allocated = g_malloc0 (private_size + alloc_size);
+      else
+        allocated = g_malloc (private_size + alloc_size);
+    }
+
+#ifdef G_ENABLE_DEBUG
+  g_ref_register (allocated + private_size);
+#endif
+
+  real = (GRef *) allocated;
+  real->ref_count = 1;
+  real->notify = notify;
+  real->alloc_size = alloc_size;
+  real->is_atomic = atomic;
+
+  return allocated + private_size;
+}
+
+/**
+ * g_ref_alloc:
+ * @alloc_size: the number of bytes to allocate
+ * @notify: (nullable): a function to be called when the reference
+ *   count drops to zero
+ *
+ * Allocates a reference counted memory area.
+ *
+ * Reference counted memory areas are automatically freed when their
+ * reference count drops to zero.
+ *
+ * Use g_ref_acquire() to acquire a reference, and g_ref_release()
+ * to release it.
+ *
+ * The contents of the returned memory are undefined.
+ *
+ * Returns: a pointer to the allocated memory
+ *
+ * Since: 2.44
+ */
+gpointer
+g_ref_alloc (gsize          alloc_size,
+             GDestroyNotify notify)
+{
+  return g_ref_alloc_internal (alloc_size, FALSE, FALSE, notify);
+}
+
+/**
+ * g_ref_alloc0:
+ * @alloc_size: the number of bytes to allocate
+ * @notify: (nullable): a function to be called when the reference
+ *   count drops to zero
+ *
+ * Allocates a reference counted memory area.
+ *
+ * Reference counted memory areas are automatically freed when their
+ * reference count drops to zero.
+ *
+ * Use g_ref_acquire() to acquire a reference, and g_ref_release()
+ * to release it.
+ *
+ * The contents of the returned memory are set to 0.
+ *
+ * Returns: a pointer to the allocated memory
+ *
+ * Since: 2.44
+ */
+gpointer
+g_ref_alloc0 (gsize          alloc_size,
+              GDestroyNotify notify)
+{
+  return g_ref_alloc_internal (alloc_size, FALSE, TRUE, notify);
+}
+
+/**
+ * g_atomic_ref_alloc:
+ * @alloc_size: the number of bytes to allocate
+ * @notify: (nullable): a function to be called when the reference
+ *   count drops to zero
+ *
+ * Allocates a atomically reference counted memory area.
+ *
+ * Reference counted memory areas are automatically freed when their
+ * reference count drops to zero.
+ *
+ * Use g_ref_acquire() to acquire a reference, and g_ref_release()
+ * to release it. References are acquired and released using the
+ * atomic primitives available on the system.
+ *
+ * The contents of the returned memory are undefined.
+ *
+ * Returns: a pointer to the allocated memory
+ *
+ * Since: 2.44
+ */
+gpointer
+g_atomic_ref_alloc (gsize          alloc_size,
+                    GDestroyNotify notify)
+{
+  return g_ref_alloc_internal (alloc_size, TRUE, FALSE, notify);
+}
+
+/**
+ * g_atomic_ref_alloc0:
+ * @alloc_size: the number of bytes to allocate
+ * @notify: (nullable): a function to be called when the reference
+ *   count drops to zero
+ *
+ * Allocates a atomically reference counted memory area.
+ *
+ * Reference counted memory areas are automatically freed when their
+ * reference count drops to zero.
+ *
+ * Use g_ref_acquire() to acquire a reference, and g_ref_release()
+ * to release it. References are acquired and released using the
+ * atomic primitives available on the system.
+ *
+ * The contents of the returned memory are set to 0.
+ *
+ * Returns: a pointer to the allocated memory
+ *
+ * Since: 2.44
+ */
+gpointer
+g_atomic_ref_alloc0 (gsize          alloc_size,
+                     GDestroyNotify notify)
+{
+  return g_ref_alloc_internal (alloc_size, TRUE, TRUE, notify);
+}
+
+/**
+ * g_ref_acquire:
+ * @ref: a reference counted memory area
+ *
+ * Acquires a reference on the given memory area.
+ *
+ * Returns: the memory area, with its reference count increased by 1
+ *
+ * Since: 2.44
+ */
+gpointer
+g_ref_acquire (gpointer ref)
+{
+  GRef *real = G_REF (ref);
+
+  g_return_val_if_fail (ref != NULL, NULL);
+#ifdef G_ENABLE_DEBUG
+  g_return_val_if_fail (g_is_ref (ref), ref);
+#endif
+
+  if (real->is_atomic)
+    g_atomic_int_add (&real->ref_count, 1);
+  else
+    real->ref_count += 1;
+
+  return ref;
+}
+
+/**
+ * g_ref_release:
+ * @ref: a reference counted memory area
+ *
+ * Releases a reference acquired using g_ref_acquire().
+ *
+ * If the reference count drops to zero, the memory area is freed.
+ *
+ * Since: 2.44
+ */
+void
+g_ref_release (gpointer ref)
+{
+  GRef *real = G_REF (ref);
+
+  g_return_if_fail (ref != NULL);
+
+  if (real->is_atomic)
+    {
+      if (g_atomic_int_dec_and_test (&real->ref_count))
+        g_ref_destroy (ref);
+    }
+  else
+    {
+      real->ref_count -= 1;
+
+      if (real->ref_count == 0)
+        g_ref_destroy (ref);
+    }
+}
